b9e82d232673e6eb277ec5ca128c0d58ab900dc8
Tomer Ady <tomerady@gmail.com>
2024-07-16 16:05:54 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./assets/templates/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ['verela round', 'big noodle']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
