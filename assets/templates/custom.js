765695817d21a35c6a8edd52fb0a6546ead2d124
Tomer Ady <tomerady@gmail.com>
2024-07-17 12:32:39 +0300

    made the layout look more neat

--- assets/templates/custom.js
+++ assets/templates/custom.js
@@ -3,8 +3,8 @@ var HEROES; // Declare a global variable
 $.getJSON("heroes.json", function(data) {
     HEROES = data; // Assign the retrieved data to the global variable
     const COLORS = [
-        "bg-blue-500",
-        "bg-red-500",
+        "bg-blue-800",
+        "bg-red-800",
     ]
     // Code to handle the retrieved data goes here
     console.log(random_hero()); // Example: Log the data to the console
@@ -13,10 +13,12 @@ $.getJSON("heroes.json", function(data) {
         for (let slot=0; slot<5; slot++) {
             var hero = random_hero();
             html += `
-            <div class="${COLORS[team]}">
-                <img src="${hero["picture"]}" alt="${hero["title"]}" class="border-4">
-                <h2 class="text-white text-lg font-bold mt-2">${hero["title"]}</h2>
-                <img src="../imgs/roles/${hero["role"]}.svg" alt="${hero["role"]} role" class="w-6 h-6 mt-2">
+            <div class="rounded-xl ${COLORS[team]}">
+                <img src="${hero["picture"]}" alt="${hero["title"]}" class="rounded-xl border-4 bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-zinc-900 via-transparent to-zinc-900"">
+                <div class="flex justify-center items-center">
+                    <h2 class="text-white text-lg font-bold my-2 italic">${hero["title"].replace("-", " ")}</h2>
+                    <img src="../imgs/roles/${hero["role"]}.svg" alt="${hero["role"]} role" class="w-8 h-8 my-2 p-1">
+                </div>
             </div>`;
             console.log(random_hero());
         }
